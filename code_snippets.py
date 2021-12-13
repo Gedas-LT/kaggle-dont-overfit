f0017074c6a7ae3d89e040480893b28340dfbf5f
Gediminas Šimavičius <gediminas.simavicius@gmail.com>
2021-12-13 23:45:47 +0200

    Reached we score. Left GridSearchCV to do

--- code_snippets.py
+++ code_snippets.py
@@ -1,18 +1,18 @@
 # Additional code snippet used for finding optimal threshold value.
 
-threshold =[i * 0.1 for i in range(1, 19)]
+threshold =[i * 0.1 for i in range(1, 31)]
 
 for i in threshold:
 
-    forest_pipe = Pipeline(
+    log_pipe = Pipeline(
         steps=[
-            ("selector", SelectFromModel(estimator=forest, threshold=f"{i}*mean")),
-            ("model", forest)
+            ("selector", SelectFromModel(estimator=log_reg, threshold=f"{i}*mean")),
+            ("model", log_reg)
         ]
     )
 
-    forest_pipe.fit(X_train, y_train)
-    predictions = forest_pipe.predict(X_test)
+    log_pipe.fit(X_train, y_train)
+    predictions = log_pipe.predict_proba(X_test)
 
-    output = pd.DataFrame({"id": test["id"], "target": predictions})
+    output = pd.DataFrame({"id": test["id"], "target": predictions[:, 1]})
     output.to_csv(f"submissions/temp/forest_{i}_mean.csv", index=False)
